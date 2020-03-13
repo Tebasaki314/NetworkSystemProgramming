60c42f6128a080f32efed92fe77be49266c9b3a4
tebasaki <h.takemura480@gmail.com>
2020-03-13 17:54:36 +0900

    implement cmd

---
+++ cmd.h
@@ -0,0 +1,6 @@
+int DoCmdArp(char **cmdline);
+int DoCmdPing(char **cmdline);
+int DoCmdIfconfig(char **cmdline);
+int DoCmdNetStat(char **cmdline);
+int DoCmdEnd(char **cmdline);
+int DoCmd(char *cmd);
